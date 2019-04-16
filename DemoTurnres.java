c841c1f10e7f352bb28aafb71c71cb7995900fa2
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-04-16 23:20:08 +0430

    add new class "DemoTurners"

---
+++ DemoTurnres.java
@@ -0,0 +1,11 @@
+public class DemoTurnres {
+    public static void main(String[] args) {
+        Leaf leaf=new Leaf();
+        Page page=new Page();
+        Pancake pancake=new Pancake();
+
+        leaf.turn();
+        page.turn();
+        pancake.turn();
+    }
+}
