ee27f316c71034732d76080412cdeea3d019fd33
kazemian <mkzn94@gmail.com>
2019-04-17 19:05:09 +0430

    Fixes DemoTurners2 class

---
+++ DemoTurners2.java
@@ -0,0 +1,16 @@
+public class DemoTurnres2 {
+    public static void main(String[] args) {
+        
+        Leaf leaf = new Leaf();
+        Page page = new Page();
+        Pancake pancake = new Pancake();
+        Key key = new Key();
+        ThermostatKnob thermostatKnob = new ThermostatKnob();
+
+        leaf.turn();
+        page.turn();
+        pancake.turn();
+        key.turn();
+        thermostatKnob.turn();
+    }
+}
